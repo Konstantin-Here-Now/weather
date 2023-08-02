438e7fe562070cb6ab0b390542d4aab96db65f0c
Konstantin-Here-Now <tyujas@gmail.com>
2023-08-02 11:26:53 +0300

    Added output formatting

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
-OPEN_WEATHER_URL_TEMPLATE = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid=" + OPEN_WEATHER_API_KEY
+OPEN_WEATHER_URL_TEMPLATE = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid=" + OPEN_WEATHER_API_KEY + \
+                            "&units=metric&lang=ru"
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
--- weather_formatter.py
+++ weather_formatter.py
@@ -2,4 +2,8 @@ from weather_api import Weather
 
 
 def format_weather(weather: Weather) -> str:
-    return weather
+    return (f"{weather.city}\n"
+            f"{weather.weather_type.value}\n"
+            f"Температура: {weather.temperature} °C\n"
+            f"Восход: {weather.sunrise.strftime('%H:%M')}\n"
+            f"Закат: {weather.sunset.strftime('%H:%M')}\n")
